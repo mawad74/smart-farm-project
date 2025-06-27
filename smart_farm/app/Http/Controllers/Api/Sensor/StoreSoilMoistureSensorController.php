ed54e4f9c1d48a46ed4017043320522150c29651
mawad74 <mohamedahmedawad77@gmail.com>
2025-06-27 04:06:40 +0300

    Add custom changes to feature/smart-farm-sensors-integration

---
+++ smart_farm/app/Http/Controllers/Api/Sensor/StoreSoilMoistureSensorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\Sensor;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Sensor\StoreSensorDataRequest;
+use App\Models\Farm;
+use App\Services\Sensor\SensorService;
+use Illuminate\Http\JsonResponse;
+
+class StoreSoilMoistureSensorController extends Controller
+{
+    public function __invoke(StoreSensorDataRequest $request): JsonResponse
+    {
+        SensorService::make(Farm::first())
+            ->setOrCreateSoilMoistureSensor()
+            ->storeSensorData($request->validated()['value']);
+
+        return response()->json([
+            'status' => 'success',
+        ]);
+    }
+}
--- smart_farm/app/Models/Sensor.php
+++ smart_farm/app/Models/Sensor.php
@@ -11,22 +11,35 @@ class Sensor extends Model
     protected $fillable = [
         'type',
         'name',
+        'device_id',
         'unit',
         'farm_id',
         'plant_id',
+        'type',
+        'value',
         'status',
+        'location',
+        'light_intensity',
     ];
 
+    protected $casts = [
+        'value' => 'float',
+        'light_intensity' => 'float',
+    ];
+
+    public function farm()
     public function farm(): BelongsTo
     {
         return $this->belongsTo(Farm::class);
     }
 
+    public function plant()
     public function plant(): BelongsTo
     {
         return $this->belongsTo(Plant::class);
     }
 
+    public function sensorData()
     public function sensorData(): HasMany
     {
         return $this->hasMany(SensorData::class);
